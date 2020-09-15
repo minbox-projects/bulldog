f7d7bdf7a0b44f28bc5f76f6007f3ba955c70246
hengboy <jnyuqy@gmail.com>
2020-09-15 14:59:17 +0800

    :tada: 创建日志采集依赖模块

---
+++ bulldog-collect/src/main/java/org/minbox/framework/bulldog/collect/LogCollector.java
@@ -0,0 +1,12 @@
+package org.minbox.framework.bulldog.collect;
+
+/**
+ * Define the log collection function
+ * <p>
+ * Provide a method of log collection, which is implemented by different frameworks
+ *
+ * @author 恒宇少年
+ */
+public interface LogCollector {
+    // ...
+}
