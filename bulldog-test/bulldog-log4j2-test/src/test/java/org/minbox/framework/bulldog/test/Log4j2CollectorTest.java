a44b9089474da6a7f9cd4dd0ea0f83133d13394c
hengboy <jnyuqy@gmail.com>
2020-09-21 13:33:31 +0800

    :white_check_mark: 新增log4j2方式单元测试示例

--- bulldog-test/bulldog-log4j2-test/src/test/java/org/minbox/framework/bulldog/test/Log4j2CollectorTest.java
+++ bulldog-test/bulldog-log4j2-test/src/test/java/org/minbox/framework/bulldog/test/Log4j2CollectorTest.java
@@ -1,29 +1,24 @@
 package org.minbox.framework.bulldog.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.minbox.framework.bulldog.collect.LogContainer;
 import org.minbox.framework.fulldog.core.pojo.GlobalLogs;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * Logback方式采集日志测试
+ * Log4j2采集日志测试
  *
  * @author 恒宇少年
  */
-public class LogbackCollectorTest {
-    /**
-     * logger instance
-     */
-    static Logger logger = LoggerFactory.getLogger(LogCollectorTest.class);
-
+@Slf4j
+public class Log4j2CollectorTest {
     @Test
-    public void collectLog() {
-        logger.info("info级别日志");
+    public void log4jTest() {
+        log.info("info级别日志");
         try {
             int a = 1 / 0;
         } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         }
         GlobalLogs globalLogs = LogContainer.getGlobalLogs();
         if (globalLogs != null) {
